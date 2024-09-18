e45e45a234f716393a2ffd7222f71a6404819aea
tetsugo-mac <tetsugou2002@gmail.com>
2024-09-19 01:02:55 +0900

    TodosコントローラーにCRUD操作を追加し、ルートを更新

--- src/DB_controllers/todocontroller.ts
+++ src/DB_controllers/todocontroller.ts
@@ -9,14 +9,59 @@ export async function getTodos(req: Request, res: Response): Promise<void> {
     res.status(500).json({ message: "Error fetching todos" });
   }
 }
+export async function getTodoById(req: Request, res: Response) {
+  try {
+    const { id } = req.params;
+    const todo = await Todos.findById(id);
+    if (!todo) {
+      res.status(404).json({ message: "Todo not found" });
+    }
+    res.json({ todo });
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching todo" });
+  }
+}
 
 export async function createTodo(req: Request, res: Response): Promise<void> {
   try {
     const { name, description, completed } = req.body;
     const newTodo = new Todos({ name, description, completed });
-    await newTodo.save();
-    res.status(201).json({ message: "Todo created" });
+    const savedTodo = await newTodo.save();
+    res.status(201).json({ message: `Todo created:${savedTodo}` });
   } catch (error) {
     res.status(500).json({ message: "Error creating todo" });
+  }
+}
+
+export async function updateTodo(req: Request, res: Response) {
+  try {
+    const { id } = req.params;
+    const { name, description, completed } = req.body;
+    const updatedTodo = await Todos.findByIdAndUpdate(id, {
+      name,
+      description,
+      completed,
+    });
+
+    if (!updateTodo) {
+      res.status(404).json({ message: "Todo not found" });
+    }
+    res.json({ message: "Todo updated", updatedTodo });
+  } catch (error) {
+    res.status(500).json({ message: "Error updating todo" });
+  }
+}
+
+export async function deleteTodo(req: Request, res: Response) {
+  try {
+    const { id } = req.params;
+    const deletedTodo = await Todos.findByIdAndDelete(id);
+
+    if (!deletedTodo) {
+      res.status(404).json({ message: "Todo not found" });
+    }
+    res.json({ message: "Todo deleted", deletedTodo });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting todo" });
   }
 }
--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import Todos from "../models/Todos";
-import { getTodos, createTodo } from "../DB_controllers/todocontroller";
+import { getTodos, createTodo, updateTodo, deleteTodo, getTodoById } from "../DB_controllers/todocontroller";
 const router = Router();
 
 router.get("/", getTodos);
+router.get("/:id", getTodoById); //指定idのtodoを取得
 router.post("/", createTodo);
+router.put("/:id", updateTodo);
+router.delete("/:id", deleteTodo);
 
 export default router;
